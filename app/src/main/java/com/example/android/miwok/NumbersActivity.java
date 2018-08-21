682ac16fd1ecd1929e7d41a5d7a3ca52f280a0e8
Hon-Jang Yang <hjyanghj@gmail.com>
2018-08-21 14:49:09 +0800

    Add feature : play audio dynamically Word add field mSoundResourceId listView.setOnItemClickListener, MediaPlayer.create, mediaPlayer.start() NumbersActivity.this, mediaPlayer.setOnCompletionListener

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,40 +1,64 @@
 package com.example.android.miwok;
 
 import android.graphics.drawable.Drawable;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
-
+    private MediaPlayer mMediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-        ArrayList<Word> words=new ArrayList<>();
-        words.add(new Word("lutti","一",R.drawable.number_one));
-        words.add(new Word("otiiko","二",R.drawable.number_two));
-        words.add(new Word("tolookosu","三",R.drawable.number_three));
-        words.add(new Word("oyyisa","四",R.drawable.number_four));
-        words.add(new Word("massokka","五",R.drawable.number_five));
-        words.add(new Word("temmokka","六",R.drawable.number_six));
-        words.add(new Word("kenekaku","七",R.drawable.number_seven));
-        words.add(new Word("kawinta","八",R.drawable.number_eight));
-        words.add(new Word("wo’e","九",R.drawable.number_nine));
-        words.add(new Word("na’aacha","十",R.drawable.number_ten));
+        final ArrayList<Word> words=new ArrayList<>();
+        words.add(new Word("lutti","一",R.drawable.number_one,R.raw.number_one));
+        words.add(new Word("otiiko","二",R.drawable.number_two,R.raw.number_two));
+        words.add(new Word("tolookosu","三",R.drawable.number_three,R.raw.number_three));
+        words.add(new Word("oyyisa","四",R.drawable.number_four,R.raw.number_four));
+        words.add(new Word("massokka","五",R.drawable.number_five,R.raw.number_five));
+        words.add(new Word("temmokka","六",R.drawable.number_six,R.raw.number_six));
+        words.add(new Word("kenekaku","七",R.drawable.number_seven,R.raw.number_seven));
+        words.add(new Word("kawinta","八",R.drawable.number_eight,R.raw.number_eight));
+        words.add(new Word("wo’e","九",R.drawable.number_nine,R.raw.number_nine));
+        words.add(new Word("na’aacha","十",R.drawable.number_ten,R.raw.number_ten));
 
         WordAdapter itemsAdapter = new WordAdapter(this, words,R.color.category_numbers);
 
         ListView listView = (ListView) findViewById(R.id.list);
-
         listView.setAdapter(itemsAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // When clicked, show a toast with the TextView text or do whatever you need.
+                //LinearLayout layout=(LinearLayout)view;
+                //TextView v=(TextView)layout.findViewById(R.id.miwok_text_view);
+                //Toast.makeText(NumbersActivity.this, v.getText(), Toast.LENGTH_SHORT).show();
+                Word currentWord = words.get(position);
+                //Toast.makeText(NumbersActivity.this, currentWord.getDefaultTranslation(), Toast.LENGTH_SHORT).show();
+                //if (mMediaPlayer!=null) mMediaPlayer.release();
+                mMediaPlayer=MediaPlayer.create(NumbersActivity.this,currentWord.getSoundResourceId());
+                mMediaPlayer.start();
+                mMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mp) {
+                        mp.release();
+                    }
+                });
+
+            }
+        });
 
         /* LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
         for (int i=0;i<words.size();i++){
--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -4,6 +4,7 @@ public class Word {
     private String mMiwokTranslation;
     private String mDefaultTranslation;
     private int mImageResourceId = NO_IMAGE;
+    private int mSoundResourceId;
     private static final int NO_IMAGE = -1;
 
     /**
@@ -23,14 +24,29 @@ public class Word {
     public int getImageResourceId(){
         return mImageResourceId;
     }
-    public Word(String miwokTranslation, String defaultTranslation){
+    public int getSoundResourceId(){
+        return mSoundResourceId;
+    }
+    public Word(String miwokTranslation, String defaultTranslation,int soundResourceId){
         mMiwokTranslation=miwokTranslation;
         mDefaultTranslation=defaultTranslation;
+        mSoundResourceId=soundResourceId;
     }
-    public Word(String miwokTranslation, String defaultTranslation, int imageResourceId){
+    public Word(String miwokTranslation, String defaultTranslation, int imageResourceId, int soundResourceId){
         mMiwokTranslation=miwokTranslation;
         mDefaultTranslation=defaultTranslation;
         mImageResourceId=imageResourceId;
+        mSoundResourceId=soundResourceId;
+    }
+
+    @Override
+    public String toString() {
+        return "Word{" +
+                "mMiwokTranslation='" + mMiwokTranslation + '\'' +
+                ", mDefaultTranslation='" + mDefaultTranslation + '\'' +
+                ", mImageResourceId=" + mImageResourceId +
+                ", mSoundResourceId=" + mSoundResourceId +
+                '}';
     }
 
     public boolean hasImage(){
